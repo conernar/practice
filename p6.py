05a6e45caf85da4e1db2d15393a0f47f170cfbd7
zkh2026 <22145235@usx.edu.cn>
2026-06-04 23:59:20 +0800

    p6

---
+++ p6.py
@@ -0,0 +1,56 @@
+class Solution1:
+    def encode(self, strs: list[str]) -> str:
+        res = ""
+        for s in strs:
+            res += (chr(666) + s)
+
+        return res
+
+
+
+    def decode(self, s: str) -> list[str]:
+        if s == "":
+            return []
+        res = []
+        temp_str = ""
+        for ss in s[1:]:
+            if ord(ss) == 666:
+                res.append(temp_str)
+                temp_str = ""
+            else:
+                temp_str += ss
+        
+        res.append(temp_str)
+        return res
+
+class Solution:
+    def encode(self, strs: list[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + '#' + s
+        return res
+
+
+    def decode(self, s: str) -> list[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            length = 0
+            while s[i] != '#':
+                length += length * 10 + ord(s[i]) - ord('0')
+                i += 1
+            i += 1
+            res.append(s[i : i + length])
+            i += length
+
+        return res
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    inputstr = ["hello", " ", "world"]
+    encoded_str = sol.encode(inputstr)
+    decoded_str = sol.decode(encoded_str)
+
+    print(decoded_str)
